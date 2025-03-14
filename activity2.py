bd158fab02f3588e5f37b41462bca0cb42c97f2c
zaya242 <isiomajadesimi@gmail.com>
2025-03-14 18:04:44 +0100

    activity2.py

---
+++ activity2.py
@@ -0,0 +1,10 @@
+
+def myfun(n):
+    iterations=0
+    for i in range(1,n+1):
+        iterations+=1
+    print("When n is ",n,"No of iterations is",iterations)    
+
+myfun(10)
+myfun(40)
+myfun(70)    
