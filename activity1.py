9d198fbff36998361d0ddc397583561b5018ef4a
zaya242 <isiomajadesimi@gmail.com>
2025-03-14 17:58:33 +0100

    activity1.py

---
+++ activity1.py
@@ -0,0 +1,10 @@
+
+def myfun(n):
+    iterations=0
+    print("The number entered by the user",n)
+    iterations+=1
+    print("No of iterations is ",iterations)
+
+myfun(10)
+myfun(40)
+myfun(37)    
